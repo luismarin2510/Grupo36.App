47d9dc23eb81ee9ad88d87766415c358b2558853
Luismarin2510 <opraedu@gmail.com>
2022-08-29 20:16:38 -0500

    4

--- Grupo36.App.Persistencia/AppRepositorio/Repositorio.cs
+++ Grupo36.App.Persistencia/AppRepositorio/Repositorio.cs
@@ -29,10 +29,29 @@ namespace Grupo36.App.Persistencia
             }
             appcox.personas.Remove(encontrarPersona);
             appcox.SaveChange();
-
-            {
-                
-            }
         }
+
+        // Llamar todos los datos de l tabla 
+
+        IEnumerable<Persona> IRepositorio.GetAllPersona(){
+             return appcox.personas;
+        }
+        // Llamar a una sola persona en especifico
+
+        Persona IRepositorio.GetPersona(int idPersona){
+            return appcox.personas.FirstOrDefault(p => p.Id == idPersona);
+        }
+        // Actualizar
+        Persona IRepositorio.UpdatePersona(Grupo36.App.Dominio.Persona perso){
+            var PersonaEncontrada = appcox.FirtsOrDefault(perso => perso.Id == perso.Id);
+
+            if(PersonaEncontrada != null){
+                PersonaEncontrada.nombre = perso.nombre;
+                appcox.SaveChanges();
+            }
+            return PersonaEncontrada;
+        }
+        
     }
+
 }
